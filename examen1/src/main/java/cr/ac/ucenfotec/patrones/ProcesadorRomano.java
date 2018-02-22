2eac7eabd7171e78138713aeec2ff7b2054f906f
pablo barquero morales <pbarquerom@ucenfotec.ac.cr>
2018-02-21 20:12:58 -0600

    Se incluye la evaluacion del la cantidad de ocurrencias para el caracter "I" y que lanze una excepcion en caso de que el conteo sea mayor a 3. Se incluye la prueba para evaluar 3 caracteres "III" y la prueba de la excepcion en caso de que el string sea de "IIII"

--- examen1/src/main/java/cr/ac/ucenfotec/patrones/ProcesadorRomano.java
+++ examen1/src/main/java/cr/ac/ucenfotec/patrones/ProcesadorRomano.java
@@ -1,8 +1,16 @@
 package cr.ac.ucenfotec.patrones;
 
+import org.springframework.util.StringUtils;
+
 public class ProcesadorRomano {
-	public int convierte(String numRomano) {
+	public int convierte(String numRomano) throws Exception {
 		int equivalente = 0;
+		
+		int ocurrenciasDeI = StringUtils.countOccurrencesOf(numRomano, "I");
+		if(ocurrenciasDeI > 3) {
+			throw new Exception();
+		}
+		
 		String[] numRomanoParts = numRomano.split("");
 		for(String numRomanoDigit : numRomanoParts) {
 			if(numRomanoDigit.equals("I")) {
--- examen1/src/test/java/cr/ac/ucenfotec/patrones/ProcesadorRomanoTest.java
+++ examen1/src/test/java/cr/ac/ucenfotec/patrones/ProcesadorRomanoTest.java
@@ -11,21 +11,33 @@ import org.junit.Assert;
 public class ProcesadorRomanoTest {
 	
 	@Test
-	public void stringVacioTest() {
+	public void stringVacioTest() throws Exception {
 		ProcesadorRomano procesador = new ProcesadorRomano();
 		Assert.assertEquals(0, procesador.convierte(""));
 	}
 	
 	@Test
-	public void stringDeUnCharTest() {
+	public void stringDeUnCharTest() throws Exception {
 		ProcesadorRomano procesador = new ProcesadorRomano();
 		Assert.assertEquals(1, procesador.convierte("I"));
 	}
 	
 	@Test
-	public void stringDeDosCharTest() {
+	public void stringDeDosCharTest() throws Exception {
 		ProcesadorRomano procesador = new ProcesadorRomano();
 		Assert.assertEquals(2, procesador.convierte("II"));
+	}
+	
+	@Test
+	public void stringDeTresCharTest() throws Exception {
+		ProcesadorRomano procesador = new ProcesadorRomano();
+		Assert.assertEquals(3, procesador.convierte("III"));
+	}
+	
+	@Test(expected = Exception.class)
+	public void stringDeCuatroCharTest() throws Exception {
+		ProcesadorRomano procesador = new ProcesadorRomano();
+		procesador.convierte("IIII");
 	}
 
 }
